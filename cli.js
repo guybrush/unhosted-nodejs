810180f270a6ef6f7f2988cec1aba93d46959c4f
Patrick Pfeiffer <patrick@buzzle.at>
2010-12-27 06:33:46 +0100

    cli.js typo, 4space indent

--- cli.js
+++ cli.js
@@ -17,50 +17,50 @@ var app      = require('./app.js')
 if (!args.length) abort(usage);
   
 while (args.length) {
-  var arg = args.shift();
-  switch (arg) {
-    case '-h':
-    case '--help':
-      abort(usage);
-      break;
-    case '-v':
-    case '--version':
-      abort(version);
-      break;
-    case '-p':                   
-    case '--port':
-      args.length
-        ? (port = args.shift())
-        : abort('--port requires an argument');
-      break;
-    case '-H':
-    case '--host':
-      args.length
-        ? (host = args.shift())
-        : abort('--host requires an argument');        
-      break;
-    case '-s':
-    case '--store':
-      args.length
-        ? (host = args.shift())
-        : abort('--store requires an argument');        
-      break;
-    case '-c': // unhostedConfig.json
-    case '--config':
-      args.length
-        ? (host = args.shift())
-        : abort('--config requires an argument');        
-      break;
-    default:break;
-  }
+    var arg = args.shift();
+    switch (arg) {
+        case '-h':
+        case '--help':
+            abort(usage);
+            break;
+        case '-v':
+        case '--version':
+            abort(version);
+            break;
+        case '-p':                   
+        case '--port':
+            args.length
+                ? (port = args.shift())
+                : abort('--port requires an argument');
+            break;
+        case '-H':
+        case '--host':
+            args.length
+                ? (host = args.shift())
+                : abort('--host requires an argument');        
+            break;
+        case '-s':
+        case '--store':
+            args.length
+                ? (store = args.shift())
+                : abort('--store requires an argument');        
+            break;
+        case '-c': // unhostedConfig.json
+        case '--config':
+            args.length
+                ? (host = args.shift())
+                : abort('--config requires an argument');        
+            break;
+        default:break;
+    }
 }
 
 app.listen(port, host);
 console.log('unhosted running on '+host+':'+port);
 
 function abort(str) {
-  console.error(str);
-  process.exit(1);
+    console.error(str);
+    process.exit(1);
 };
 
 
